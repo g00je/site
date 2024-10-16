f37c588c2b60cbcac5676d1bc7afca546fab95a0
sadra <sadrataghavi1383@gmail.com>
2024-10-16 18:25:09 +0330

    release: 0.12.2

--- web/script/blog.ts
+++ web/script/blog.ts
@@ -97,12 +97,6 @@ document.onscroll = e => {
         addonSvg1.style.opacity = `0`
         addonSvg2.style.opacity = `0`
     }
-
-    if (percentage >= 50) {
-        blogIntro.style.visibility = 'hidden'
-    } else {
-        blogIntro.style.visibility = 'visible'
-    }
 }
 
 // tools
